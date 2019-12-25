a3f06a237a0736dfe4ead83098f6ed3dea18a1e8
tomaszleszczynski1980 <lin.fowen@gmail.com>
2019-12-25 01:19:18 +0100

    colors.py extended whith new values

--- colors.py
+++ colors.py
@@ -1,13 +1,19 @@
 class colors:
+    
+    WHITE = '\033[97m'
+    AQUA = '\033[96m'
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'  
+    RED = '\033[91m'
+    GREY = '\033[90m'
 
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[9m'
+    BOLD = '\033[1m'
+    WEAK = '\033[2m'
+    ITALIC = '\033[3m'
     UNDERLINE = '\033[4m'
     BLINK = '\033[5m'
-
+    BACKGROUND = '\033[7m'
 
