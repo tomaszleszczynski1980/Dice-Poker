e487403370749889da1b645129877ab12dce1547
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-01-08 15:20:38 +0100

    players_dictionary.py poczyszczony zgonie ze wskazówkami Mikołaja, Check_figures.py jeszcze nie kopletne

--- Check_figures.py
+++ Check_figures.py
@@ -1,100 +1,108 @@
-def check_hand(hand: list, points: dict):
-    '''this function checks avaiable figures in hand in five dice Poker'''
 
-    results = []
+def is_poker (sorted_hand: list):
 
-    sorted_hand = sorted (hand)
+    bonus = 50
 
-
-    def is_poker (sorted_hand: list):
-
-        bonus = 50
-
-        if sorted_hand.count(sorted_hand[0]) == 5:
-            return ('Five of a kind', sum(sorted_hand) + bonus)
+    if sorted_hand.count(sorted_hand[0]) == 5:
+        return ('Five of a kind', sum(sorted_hand) + bonus)
 
 
-    def is_four (sorted_hand: list):
+def is_four (sorted_hand: list):
 
-        bonus = 20
+    bonus = 20
 
-        if (sorted_hand.count(sorted_hand[0]) >= 4):
-            return ('Four of a kind', bonus + sum(sorted_hand[0:4]))
+    if (sorted_hand.count(sorted_hand[0]) >= 4):
+        return ('Four of a kind', bonus + sum(sorted_hand[0:4]))
 
-        elif (sorted_hand.count(sorted_hand[-1]) >= 4):
-            return ('Four of a kind', bonus + sum(sorted_hand[1:5]))
-
-
-    def is_fullhouse (sorted_hand: list):
-
-        bonus = 10
-
-        if ((sorted_hand.count(sorted_hand[0]) == 3) and (sorted_hand.count(sorted_hand[-1]) == 2)):
-            return ('Full House', bonus + sum(sorted_hand))
-
-        elif ((sorted_hand.count(sorted_hand[0]) == 2) and (sorted_hand.count(sorted_hand[-1]) == 3)):
-            return ('Full House', bonus + sum(sorted_hand))
+    elif (sorted_hand.count(sorted_hand[-1]) >= 4):
+        return ('Four of a kind', bonus + sum(sorted_hand[1:5]))
 
 
-    def is_lstraight (sorted_hand: list):
+def is_fullhouse (sorted_hand: list):
+
+    bonus = 10
+
+    if ((sorted_hand.count(sorted_hand[0]) == 3) and (sorted_hand.count(sorted_hand[-1]) == 2)):
+        return ('Full House', bonus + sum(sorted_hand))
+
+    elif ((sorted_hand.count(sorted_hand[0]) == 2) and (sorted_hand.count(sorted_hand[-1]) == 3)):
+        return ('Full House', bonus + sum(sorted_hand))
+
+
+def is_lstraight (sorted_hand: list):
 
     if sorted_hand == [2, 3, 4, 5, 6]:
         return ('Large Straight', 20)     # large straight is always 20 points
 
 
-    def is_sstraight (sorted_hand: list):
+def is_sstraight (sorted_hand: list):
 
     if sorted_hand == [1, 2, 3, 4, 5]:
         return ('Small Straight', 15)    # small straight is always 15 points
 
 
-    def is_even (sorted_hand: list):
+def is_even (sorted_hand: list):
 
     if all([dice_value for dice_value in sorted_hand if dice_value % 2 == 0]):
         return ('All even', sum(sorted_hand))
 
 
-    def is_odd (sorted_hand: list):
+def is_odd (sorted_hand: list):
 
     if all([dice_value for dice_value in sorted_hand if dice_value % 2 != 0]):
         return ('All odd', sum(sorted_hand))
 
-    # checks if is 3 of a kind
 
-    if (temp.count(temp[0]) >= 3):
-        results.append(('Three of a kind', sum(temp[0:3])))
+def is_three (sorted_hand: list):
 
-    elif (temp.count(temp[-1]) >= 3):
-        results.append(('Three of a kind', sum(temp[2:5])))
+    if (sorted_hand.count(sorted_hand[0]) >= 3):
+        return ('Three of a kind', sum(sorted_hand[0:3]))
 
-    elif (temp.count(temp[1]) >= 3):
-        results.append(('Three of a kind', sum(temp[1:4])))
+    elif (sorted_hand.count(sorted_hand[-1]) >= 3):
+        return ('Three of a kind', sum(sorted_hand[2:5]))
 
-    # checks if there are two pairs
+    elif (sorted_hand.count(sorted_hand[1]) >= 3):
+        return ('Three of a kind', sum(sorted_hand[1:4]))
 
-    if ((temp[0] == temp[1])):
 
-        if ((temp[2] == temp[3])):
-            results.append(('Two Pairs', sum(temp[0:4])))
+def is_2pairs(sorted_hand: list):
 
-        elif (temp[3] == temp[4]):
-            results.append(('Two Pairs', sum(temp) - temp[2]))
+    if ((sorted_hand[0] == sorted_hand[1])):
 
-    elif ((temp[1] == temp[2]) and (temp[3] == temp[4])):
-        results.append(('Two Pairs', sum(temp[1:5])))
+        if ((sorted_hand[2] == sorted_hand[3])):
+            return ('Two Pairs', sum(sorted_hand[0:4]))
 
-    # checks if is pair or pairs
+        elif (sorted_hand[3] == sorted_hand[4]):
+            return ('Two Pairs', sum(sorted_hand) - sorted_hand[2])
 
-    for i in range(1, 7):
-        if temp.count(i) >= 2:
-            results.append(('Pair', i * 2))
+    elif ((sorted_hand[1] == sorted_hand[2]) and (sorted_hand[3] == sorted_hand[4])):
+        return ('Two Pairs', sum(sorted_hand[1:5]))
+
+
+def is_pair(sorted_hand: list):
+
+    # dice_value
+
+    for dice_value in range(1, 7):
+        if sorted_hand.count(dice_value) >= 2:
+            sorted_hand.remove(sorted_hand[dice_value])
+            return ('Pair', dice_value * 2)
+
 
     # chance is always :), so adds it to the list results
 
     results.append(('Chance', sum(hand)))
 
-    # removes from results figures that we already achieved or striked out
-    # those figures that have value different than 0 in points dict
+
+def check_hand(hand: list, points: dict, figures_pattern: dict):
+    '''this function checks avaiable figures in hand basing on figures pattern'''
+
+    results = []
+    sorted_hand = sorted(hand)
+
+    for function in figures_pattern.values():
+        function()
+
 
     results_temp = results[:]
 
--- players_dictionary.py
+++ players_dictionary.py
@@ -1,18 +1,58 @@
+from copy import copy
 
 # global variables
-    #players dictionary
-    
-print('number of players?:')
-players_number = int(input())
-players_list = []
-for x in range(players_number):
-    players_list.append (input(f'podaj imie {x+1} gracza: '))
 
-print('\n')
-for i in range(players_number):
-    print(f'player number {i+1}:', players_list[i])
+points = {'Pair': 0,
+          'Two Pairs': 0,
+          'Three of a kind': 0,
+          'Small Straight': 0,
+          'Large Straight': 0,
+          'All even': 0,
+          'All odd': 0,
+          'Full House': 0,
+          'Four of a kind': 0,
+          'Five of a kind': 0,
+          'Chance': 0
+          }
+
+
+#players dictionary
+
+def make_players_dict(players_list: list, points: dict):
     
-players_dic = {}
-for i in range(players_number):
-    #players_dic = players_dic + {players_list[i]: i}
-    players_dic.update({players_list[i]: i+1})
+    players_dict = {}
+
+    for i in range(len(players_list)):
+        players_dic.update({players_list[i]: copy(points)})
+
+    return players_dict
+
+
+def get_players():   
+
+    players_number = int(input('number of players?: '))
+
+    players_list = []
+    for x in range(players_number):
+        players_list.append (input(f'podaj imie {x+1} gracza: '))
+
+    return players_list
+
+
+def display_players(players_list: list):
+
+    print('Players in the game:')
+    for i in range(len(players_list)):
+        print(f'player number {i+1}:', players_list[i])
+
+
+def test_this_module():
+    players_list = get_players()
+
+    print ('')
+
+    display_players(players_list)
+
+    players = make_players_dic(players_list, points)
+
+    print (players)
