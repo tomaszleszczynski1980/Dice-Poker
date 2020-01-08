5fbc755753098c062f17537bb344e6fd659b9acd
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-01-08 22:41:08 +0100

    function check_figures are ready!

--- Check_figures.py
+++ Check_figures.py
@@ -1,5 +1,4 @@
 def is_five(sorted_hand: list):
-
     bonus = 50
 
     if sorted_hand.count(sorted_hand[0]) == 5:
@@ -7,7 +6,6 @@ def is_five(sorted_hand: list):
 
 
 def is_four(sorted_hand: list):
-
     bonus = 20
 
     if (sorted_hand.count(sorted_hand[0]) >= 4):
@@ -18,7 +16,6 @@ def is_four(sorted_hand: list):
 
 
 def is_fullhouse(sorted_hand: list):
-
     bonus = 10
 
     if ((sorted_hand.count(sorted_hand[0]) == 3) and (sorted_hand.count(sorted_hand[-1]) == 2)):
@@ -29,31 +26,26 @@ def is_fullhouse(sorted_hand: list):
 
 
 def is_odd(sorted_hand: list):
-
     if all([dice_value for dice_value in sorted_hand if dice_value % 2 != 0]):
         return list(('All odd', sum(sorted_hand)))
 
 
 def is_even(sorted_hand: list):
-
     if all([dice_value for dice_value in sorted_hand if dice_value % 2 == 0]):
         return list(('All even', sum(sorted_hand)))
 
 
 def is_lstraight(sorted_hand: list):
-
     if sorted_hand == [2, 3, 4, 5, 6]:
-        return list(('Large Straight', 20))    # large straight is always 20 points
+        return list(('Large Straight', 20))  # large straight is always 20 points
 
 
 def is_sstraight(sorted_hand: list):
-
     if sorted_hand == [1, 2, 3, 4, 5]:
-        return list(('Small Straight', 15))    # small straight is always 15 points
+        return list(('Small Straight', 15))  # small straight is always 15 points
 
 
 def is_three(sorted_hand: list):
-
     if (sorted_hand.count(sorted_hand[0]) >= 3):
         return list(('Three of a kind', sum(sorted_hand[0:3])))
 
@@ -65,7 +57,6 @@ def is_three(sorted_hand: list):
 
 
 def is_2pairs(sorted_hand: list):
-
     if ((sorted_hand[0] == sorted_hand[1])):
 
         if ((sorted_hand[2] == sorted_hand[3])):
@@ -79,51 +70,52 @@ def is_2pairs(sorted_hand: list):
 
 
 def is_pair(sorted_hand: list):
-    # this function is NOT ready!
+    pairs_list = []
 
-    for dice_value in range(1, 7):
+    for dice_value in range(1, 6 + 1):
         if sorted_hand.count(dice_value) >= 2:
-            # sorted_hand.remove(sorted_hand[dice_value])
-            return ('Pair', dice_value * 2)
+            pairs_list.append(('Pair', dice_value * 2))
+
+    return pairs_list
 
 
 def is_chance(sorted_hand: list):
-    '''chance is always :), so adds it to the list results'''
-
     return list(('Chance', sum(sorted_hand)))
+
+
+figures_pattern = {'Pair': is_pair,
+                   'Two Pairs': is_2pairs,
+                   'Three of a kind': is_three,
+                   'Small Straight': is_sstraight,
+                   'Large Straight': is_lstraight,
+                   'All even': is_even,
+                   'All odd': is_odd,
+                   'Full House': is_fullhouse,
+                   'Four of a kind': is_four,
+                   'Five of a kind': is_five,
+                   'Chance': is_chance
+                   }
 
 
 # following function calls all the functions defined above according to given figures pattern
 
-figures_pattern = {'Pair': is_pair,
-          'Two Pairs': is_2pairs,
-          'Three of a kind': is_three,
-          'Small Straight': is_sstraight,
-          'Large Straight': is_lstraight,
-          'All even': is_even,
-          'All odd': is_odd,
-          'Full House': is_fullhouse,
-          'Four of a kind': is_four,
-          'Five of a kind': is_five,
-          'Chance': is_chance
-          }
-
-def check_hand(hand: list, points: dict, figures_pattern: dict):
+def check_hand(hand: list, figures_pattern: dict):
     '''this function checks avaiable figures in hand basing on figures pattern'''
-
     results = []
     sorted_hand = sorted(hand)
 
     for function in figures_pattern.values():
         results.extend(function(sorted_hand))
 
+    return results
 
-    # here we remove figures that have been scored or stroke out
 
-    results_temp = results[:]
+def remove_figures_already_got(results: list, points: dict):
+    '''this function removes figures which were already scored or stroke out'''
+    results_copy = results[:]
 
-    for z in results_temp:
-        if points[z[0]] != 0:
-            results.remove(z)
+    for result in results_copy:
+        if points[result[0]] != 0:
+            results.remove(result)
 
     return results
