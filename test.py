fa79372fca151eef4cc0235970dc949be76f796f
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-01-08 13:24:08 +0100

    zmiany w pliku test

--- test.py
+++ test.py
@@ -47,3 +47,59 @@ gracze[0]+'points' = points
 
 
 
+
+players = {'First': {'Pair': 0,
+          'Two Pairs': 0,
+          'Three of a kind': 0,
+          'Small Straight': 0,
+          'Large Straight': 0,
+          'All even': 0,
+          'All odd': 0,
+          'Full House': 0,
+          'Four of a kind': 0,
+          'Five of a kind': 0,
+          'Chance': 0
+          },
+           'Second': {'Pair': 0,
+          'Two Pairs': 0,
+          'Three of a kind': 0,
+          'Small Straight': 0,
+          'Large Straight': 0,
+          'All even': 0,
+          'All odd': 0,
+          'Full House': 0,
+          'Four of a kind': 0,
+          'Five of a kind': 0,
+          'Chance': 0
+          },
+           'Third': {'Pair': 0,
+          'Two Pairs': 0,
+          'Three of a kind': 0,
+          'Small Straight': 0,
+          'Large Straight': 0,
+          'All even': 0,
+          'All odd': 0,
+          'Full House': 0,
+          'Four of a kind': 0,
+          'Five of a kind': 0,
+          'Chance': 0
+          }
+           }
+
+
+def points_table(points_dict: dict):
+
+    print('+--------------------+--------+')
+
+    for i in points_dict.items():
+
+        print('|', i[0].ljust(18), '|', str(i[1]).center(6), '|')
+
+    print('+--------------------+--------+')
+    print('|                    |        |')
+    print('| TOTAL              |', str(sum_points(points_dict)).center(6), '|')
+    print('|                    |        |')
+    print('+--------------------+--------+')
+
+
+
