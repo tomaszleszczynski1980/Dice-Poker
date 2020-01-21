1bf6ef64f3ed84b942ba9c31386f1e23769d5838
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-01-21 11:00:31 +0100

    Added AI.py file

---
+++ DICE_POKER/AI.py
@@ -0,0 +1,25 @@
+Figures_probability = {'Pair': 0.4630,
+                   'Two Pairs': 0.2315,
+                   'Three of a kind': 0.1543,
+                   'Small Straight': 0.0309,
+                   'Large Straight': 0.0309,
+                   'All even': 0.0313,
+                   'All odd': 0.0313,
+                   'Full House': 0.0386,
+                   'Four of a kind': 0.0193,
+                   'Five of a kind': 0.0008,
+                   'Chance': 1.000
+                   }
+
+Figures_points = {'Pair': (12, 6, 6.0),
+                   'Two Pairs': (24, 4, 14.0),
+                   'Three of a kind': (18, 3, 10.5),
+                   'Small Straight': (15, 15, 15.0),
+                   'Large Straight': (20, 20, 20.0),
+                   'All even': (30, 10, 20.0),
+                   'All odd': (25, 5, 15.0),
+                   'Full House': (40, 15, 27.5),
+                   'Four of a kind': (44, 24, 34.0),
+                   'Five of a kind': (80, 55, 67.5),
+                   'Chance': (30, 5, 17.5)
+                    }
