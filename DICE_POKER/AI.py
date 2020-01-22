101aa947b51daf5586e33b2a5f5321b181c80efd
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-01-22 14:05:06 +0100

    Remove unused functions and dictionaries (they are still in Tomek branch)

--- DICE_POKER/AI.py
+++ DICE_POKER/AI.py
@@ -1,35 +1,3 @@
-Figures_probability = {'Pair': 0.4630,
-                       'Two Pairs': 0.2315,
-                       'Three of a kind': 0.1543,
-                       'Small Straight': 0.0309,
-                       'Large Straight': 0.0309,
-                       'All even': 0.0313,
-                       'All odd': 0.0313,
-                       'Full House': 0.0386,
-                       'Four of a kind': 0.0193,
-                       'Five of a kind': 0.0008,
-                       'Chance': 1.000
-                       }
-
-Figures_points = {'Pair': (12, 2, 6.0),
-                  'Two Pairs': (24, 4, 14.0),
-                  'Three of a kind': (18, 3, 10.5),
-                  'Small Straight': (15, 15, 15.0),
-                  'Large Straight': (20, 20, 20.0),
-                  'All even': (30, 10, 20.0),
-                  'All odd': (25, 5, 15.0),
-                  'Full House': (40, 15, 27.5),
-                  'Four of a kind': (44, 24, 34.0),
-                  'Five of a kind': (80, 55, 67.5),
-                  'Chance': (30, 5, 17.5)
-                  }
-
-def work_function():
-    res = {}
-    for figure, probability in Figures_probability.items():
-        relation = Figures_points[figure][2] / probability
-        res[figure] = relation
-
 """Below dictionary defines figure occurance probability in five dice throw,
    maximum figures points and remove coefficient.
    The lower coefficient is the figure is first in queue to be stroke out.
